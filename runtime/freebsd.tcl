56dfdc8298c780e6aa6a8a200d4c90f39fcb9881
Denis Salopek <denisSal@users.noreply.github.com>
2025-07-22 19:58:56 +0200

    WIP Refactor drawing pseudo nodes/links
    Remove pseudo nodes from non-GUI config.

--- runtime/freebsd.tcl
+++ runtime/freebsd.tcl
@@ -476,10 +476,6 @@ proc execSetIfcQDisc { eid node_id iface_id qdisc } {
 		DRR { set qdisc drr }
 	}
 
-	if { [getNodeType $node1_id] == "pseudo" } {
-		set link_id [getLinkMirror $link_id]
-	}
-
 	pipesExec "jexec $eid ngctl msg $link_id: setcfg \"{ $direction={ $qdisc=1 } }\"" "hold"
 }
 
@@ -508,10 +504,6 @@ proc execSetIfcQDrop { eid node_id iface_id qdrop } {
 		drop-tail { set qdrop droptail }
 	}
 
-	if { [getNodeType $node1_id] == "pseudo" } {
-		set link_id [getLinkMirror $link_id]
-	}
-
 	pipesExec "jexec $eid ngctl msg $link_id: setcfg \"{ $direction={ $qdrop=1 } }\"" "hold"
 }
 
@@ -536,10 +528,6 @@ proc execSetIfcQLen { eid node_id iface_id qlen } {
 
 	if { $qlen == 0 } {
 		set qlen -1
-	}
-
-	if { [getNodeType $node1_id] == "pseudo" } {
-		set link_id [getLinkMirror $link_id]
 	}
 
 	pipesExec "jexec $eid ngctl msg $link_id: setcfg \"{ $direction={ queuelen=$qlen } }\"" "hold"
