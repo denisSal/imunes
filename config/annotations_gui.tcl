61e585ee6f9fd4a65e9d2844acd498a766c76266
Denis Salopek <denisSal@users.noreply.github.com>
2025-07-28 19:10:51 +0200

    WIP Separate getter/setter procs
    WIP Move GUI procs to new files

--- config/annotations_gui.tcl
+++ config/annotations_gui.tcl
@@ -1,33 +1,30 @@
-#****f* annotationscfg.tcl/addAnnotation
-# NAME
-#   addAnnotation -- add annotation object
-# SYNOPSIS
-#   addAnnotation $annotation_id $type
-# FUNCTION
-#   Adds annotation object to annotation list.
-# INPUTS
-#   * annotation_id -- new annotation id
-#   * type -- annotation type
-#****
-proc addAnnotation { annotation_id type } {
-	lappendToRunning_gui "annotation_list" $annotation_id
-	setAnnotationType $annotation_id $type
-}
-
-#****f* annotations.tcl/deleteAnnotation
-# NAME
-#   deleteAnnotation -- delete annotation
-# SYNOPSIS
-#   deleteAnnotation $annotation_id $type
-# FUNCTION
-#   Deletes annotation from canvas.
-# INPUTS
-#   * annotation_id -- existing annotation
-#****
-proc deleteAnnotation { annotation_id } {
-	setToRunning_gui "annotation_list" [removeFromList [getFromRunning_gui "annotation_list"] $annotation_id]
-	cfgUnset "gui" "annotations" $annotation_id
-}
+#
+# Copyright 2025- University of Zagreb.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+# This work was supported in part by Croatian Ministry of Science
+# and Technology through the research contract #IP-2003-143.
+#
 
 #****f* annotationscfg.tcl/getAnnotationType
 # NAME
@@ -336,10 +333,13 @@ proc getAnnotationCoords { annotation_id } {
 #   * coords -- coordinates
 #****
 proc setAnnotationCoords { annotation_id coords } {
-	set roundcoords {}
 	foreach c $coords {
-		set x [expr int($c)]
+		set x [expr round($c)]
 		lappend roundcoords $x
+	}
+
+	if { $coords == {} } {
+		set roundcoords "0 0"
 	}
 
 	cfgSet "gui" "annotations" $annotation_id "iconcoords" $roundcoords
