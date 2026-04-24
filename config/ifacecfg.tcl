34d4cdff3e882d439b0036af1f3b431df941d2dd
Denis Salopek <denisSal@users.noreply.github.com>
2026-04-24 09:47:54 +0200

    Refactor IPv4/IPv6 procedures
    Lots of internal, some GUI changes:
     - enable setting increment bits when generating IPv6 subnets
     - refactor/fix subnet generation/assignment to behave more
       predictable/consistent
     - clean up code

--- config/ifacecfg.tcl
+++ config/ifacecfg.tcl
@@ -828,7 +828,7 @@ proc updateIface { node_id iface_id old_iface_cfg new_iface_cfg } {
 
 			"ipv4_addrs" {
 				if { $iface_prop_new_value == "auto" } {
-					autoIPv4addr $node_id $iface_id
+					autoIPAddr "ipv4" $node_id $iface_id
 				} else {
 					setIfcIPv4addrs $node_id $iface_id $iface_prop_new_value
 				}
@@ -836,7 +836,7 @@ proc updateIface { node_id iface_id old_iface_cfg new_iface_cfg } {
 
 			"ipv6_addrs" {
 				if { $iface_prop_new_value == "auto" } {
-					autoIPv6addr $node_id $iface_id
+					autoIPAddr "ipv6" $node_id $iface_id
 				} else {
 					setIfcIPv6addrs $node_id $iface_id $iface_prop_new_value
 				}
