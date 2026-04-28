3964c875c14c4702220ae8e1b7f492920617358b
Denis Salopek <denisSal@users.noreply.github.com>
2026-04-28 20:45:18 +0200

    <tmp> step-ify terminate procs

--- runtime/execute.tcl
+++ runtime/execute.tcl
@@ -761,7 +761,7 @@ proc deployCfg { { execute 0 } } {
 	# -> step10 - execute_nodesConfigure
 	# -> step11 - execute_finishExecution
 
-	execute_nodesCreateVirtualized $all_dict $virtualized_nodes $virtualized_nodes_count $w
+	execute_nodesCreateVirtualized $all_dict $w
 }
 
 proc execute_prepareSystem { progressbar_widget msg_widget } {
@@ -834,8 +834,11 @@ proc execute_prepareSystem { progressbar_widget msg_widget } {
 	createRunningVarsFile $eid
 }
 
-proc execute_nodesCreateVirtualized { all_dict virtualized_nodes virtualized_nodes_count w } {
+proc execute_nodesCreateVirtualized { all_dict w } {
 	global progressbarCount execMode runnable_node_types gui
+
+	set virtualized_nodes [dictGet $all_dict "virtualized_nodes"]
+	set virtualized_nodes_count [dictGet $all_dict "virtualized_nodes_count"]
 
 	statline "Instantiating VIRTUALIZED nodes..."
 	if { $virtualized_nodes_count > 0 } {
