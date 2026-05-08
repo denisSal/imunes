332ac99a21445ef1d1111ed2347f7c561a4b824f
Denis Salopek <denisSal@users.noreply.github.com>
2026-05-08 20:20:36 +0200

    <tmp> unifying all terminate procs

--- runtime/linux.tcl
+++ runtime/linux.tcl
@@ -450,6 +450,8 @@ proc configureLinkBetween { node1_id node2_id iface1_id iface2_id link_id } {
 }
 
 proc unconfigureLinkBetween { eid node1_id node2_id iface1_id iface2_id link_id } {
+	addStateLink $link_id "unconfiguring"
+
 	foreach node_id "$node1_id $node2_id" iface_id "$iface1_id $iface2_id" {
 		set public_ns [invokeNodeProc $node_id "getPublicNs" $eid $node_id]
 		lassign [invokeNodeProc $node_id "getHookData" $node_id $iface_id] - public_iface -
