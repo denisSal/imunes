754ae6185327fae5bacdeb354909118f8beb8965
Denis Salopek <denisSal@users.noreply.github.com>
2026-09-13 10:39:08 +0200

    Improve snap to grid

--- gui/drawing.tcl
+++ gui/drawing.tcl
@@ -2078,25 +2078,10 @@ proc drawGradientCircle { image_obj palette image_width image_height } {
 }
 
 proc snapCoordsToGrid { x y } {
-	global grid main_canvas_elem
+	global grid
 
-	set zoom [getActiveOption "zoom"]
+	set snapped_x [expr { round($x / $grid) * $grid }]
+	set snapped_y [expr { round($y / $grid) * $grid }]
 
-	set new_x [expr $x + {(int($x / $grid + 0.5) * $grid - $x) * $zoom}]
-	set new_y [expr $y + {(int($y / $grid + 0.5) * $grid - $y) * $zoom}]
-
-	return "[expr int(round($new_x))] [expr int(round($new_y))]"
-}
-
-proc snapObjectToGrid { image_obj } {
-	global grid main_canvas_elem
-
-	set zoom [getActiveOption "zoom"]
-
-	lassign [$main_canvas_elem coords $image_obj] x y
-	lassign [snapCoordsToGrid $x $y] x y
-
-	$main_canvas_elem coords $image_obj $x $y
-
-	return "$x $y"
+	return "[expr { int($snapped_x) }] [expr { int($snapped_y) }]"
 }
